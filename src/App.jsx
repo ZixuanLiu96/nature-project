54ccb2cb75bfabe6b463a332863a1ef46d8bfdef
Barbara Goldbeck <goldbeck.barbara@gmail.com>
2025-09-16 20:41:30 +0200

    Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
+import { Routes, Route } from "react-router-dom";
+
+
 function App() {
   return (
     <>
       <div>this is our nature project!</div>
+      <Routes>
+        <Route path="/" element={<HomePage />}></Route>
+        <Route path="/login" element={<LoginPage />}></Route>
+        <Route path="/signup" element={<SignUpPage />}></Route>
+        <Route path="/users/:id" element={<DashboardPage />}></Route>
+        <Route path="*" element={<ErrorPage />}></Route>
+        
+      </Routes>
     </>
   );
 }
