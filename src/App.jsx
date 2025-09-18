868476cd578bec2f083f7942dbfe0b5adc3fb075
Zixuan <liusherry08@gmail.com>
2025-09-18 19:07:28 +0100

    delet one line in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,22 @@
-
 import { Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
-import LoginPage from "./pages/LoginPage"; 
-import SignUpPage from "./pages/SignUpPage"; 
-import DashboardPage from "./pages/DashboardPage"; 
-import ErrorPage from "./pages/ErrorPage"; 
+import LoginPage from "./pages/LoginPage";
+import SignUpPage from "./pages/SignUpPage";
+import DashboardPage from "./pages/DashboardPage";
+import ErrorPage from "./pages/ErrorPage";
 import "./App.css";
-
 
 function App() {
   return (
     <>
-
-      <div>this is our nature project!</div>
       <Routes>
         <Route path="/" element={<HomePage />}></Route>
         <Route path="/login" element={<LoginPage />}></Route>
         <Route path="/signup" element={<SignUpPage />}></Route>
         <Route path="/users/:id" element={<DashboardPage />}></Route>
         <Route path="*" element={<ErrorPage />}></Route>
-        
       </Routes>
-   
-
-      
     </>
-
   );
 }
 
